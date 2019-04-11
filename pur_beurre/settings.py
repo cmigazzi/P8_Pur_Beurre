7bd383a8ee2c37845cf07f5efaffa0916495613f
cmigazzi <cedric.migazzi@hotmail.fr>
2019-04-11 16:04:17 +0200

    SessionMiddleware added

--- pur_beurre/settings.py
+++ pur_beurre/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
