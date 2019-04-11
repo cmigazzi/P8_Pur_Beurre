102830e250d5208487c1718e18d1cf8df36e47f4
cmigazzi <cedric.migazzi@hotmail.fr>
2019-04-11 12:33:47 +0200

    add responsiveness and display non field errors in login and signup forms

--- pur_beurre/eat_better/forms.py
+++ pur_beurre/eat_better/forms.py
@@ -5,7 +5,7 @@ class SearchForm(forms.Form):
     product = forms.CharField(
         max_length=150,
         widget=forms.TextInput(attrs={
-                    "class": "form-control mr-2 autocomplete col-lg-9 col-md-8 col-sm-6 col-xs-4 search-field",
+                    "class": "form-control mr-2 autocomplete col-lg-9 col-md-8 col-sm-10 search-field",
                     "placeholder": "Produit"})
                              )
 
@@ -15,7 +15,7 @@ class NavSearchForm(forms.Form):
         max_length=150,
         label='', 
         widget=forms.TextInput(attrs={
-                    "class": "form-control input-sm mr-sm-1 search-field",
+                    "class": "form-control mr-sm-1 search-field",
                     "type": "search",
                     "placeholder": "Chercher",
                     "aria-label": "Search"})
