3e6480fad0129bab00fddf6b3f81520effe205dd
cmigazzi <cedric.migazzi@hotmail.fr>
2019-04-11 15:59:16 +0200

    ready to deploy #1

--- pur_beurre/settings.py
+++ pur_beurre/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY', default='Qu2+ke{+Xv\ng#FVdCWr-U,4n')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', default=False)
